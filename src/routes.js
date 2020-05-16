88e7f5254c3e2de752a62ec79cc08780c14181cb
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-05-16 11:38:52 -0300

    nodemon e sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({message: 'hiiii oni chan'});
+    return res.json({message: 'lerigou'});
 })
 
-module.exports = routes;
+export default routes;
