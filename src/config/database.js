bd2b1bd6c159f8477f5ef7a497d0e8ad580bd1c2
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-05-16 19:57:14 -0300

    migration de usuários

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'postgres',
   database: 'gobarber',
+  port: 5432,
   define: {
     timestamps: true,
     underscored: true,
