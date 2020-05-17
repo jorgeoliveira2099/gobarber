9ea6dc8dcd288c0f20480c450ddd4a2e94112ab6
jorgeoliveira2099 <josuejorge2012@gmail.com>
2020-05-17 12:29:16 -0300

    add user controller

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'joojo é',
-    email: 'jooj@gmail.com',
-    password_hash: 'sdfgds67',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
